812047c81ff0485223fa809591b89ce50cb45465
Grzegorz Fic <gf9693949@gmail.com>
2022-06-15 18:12:39 +0200

    zmiana w drugim repo

--- Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
+++ Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Poland");
     }
+    private void displayText() {
+        System.out.println(zmiana w drugim repo);
+    }
 }
