d58b14e114726c70170d587e9bcde9c7e5f6abfa
Grzegorz Fic <gf9693949@gmail.com>
2022-06-15 18:19:35 +0200

    Zmiana w kodzie main.java

--- Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
+++ Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Poland");
+        System.out.println("Hello World");
     }
     private void displayText() {
         System.out.println(zmiana w drugim repo);
