c7ac2f263461269644578043c3c76f416e1ca8dd
Grzegorz Fic <gf9693949@gmail.com>
2022-06-15 18:32:28 +0200

    Hello Mariuszek

--- Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
+++ Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Kitty");
+        System.out.println("Hello Mairuszek");
     }
     private void displayText() {
         System.out.println(zmiana w drugim repozytorium);
