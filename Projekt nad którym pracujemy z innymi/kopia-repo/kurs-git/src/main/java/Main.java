ab1f9ce4c7b12c8102c259a217dc794b846ead31
Grzegorz Fic <gf9693949@gmail.com>
2022-06-15 18:30:31 +0200

    Hello Kitty

--- Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
+++ Projekt nad którym pracujemy z innymi/kopia-repo/kurs-git/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Kitty");
     }
     private void displayText() {
         System.out.println(zmiana w drugim repozytorium);
